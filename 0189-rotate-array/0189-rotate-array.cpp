6dfdd7d09cc35755a1be8e53d8d634145c09e03b
suchitashukla123 <2k23.cscys2311174@gmail.com>
2025-04-15 16:36:35 +0530

    Time: 0 ms (100%), Space: 29.4 MB (96.99%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;  // In case k is larger than n
+        std::reverse(nums.begin(), nums.end());
+        std::reverse(nums.begin(), nums.begin() + k);
+        std::reverse(nums.begin() + k, nums.end());
+    }
+};
+
