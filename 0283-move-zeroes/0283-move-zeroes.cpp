6efd039c2a4ca743414e002df94259cb095ef7f9
suchitashukla123 <2k23.cscys2311174@gmail.com>
2025-05-21 14:15:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        for(int i=j+1;i<nums.size();i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;       
+            }
+        }
+        
+    }
+};
