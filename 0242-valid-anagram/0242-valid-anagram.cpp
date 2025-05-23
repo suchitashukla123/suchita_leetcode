9788411440bb6c9f8e43a782ff5645ab4a612c24
suchitashukla123 <2k23.cscys2311174@gmail.com>
2025-05-23 22:39:28 +0530

    Time: 3 ms (53.29%), Space: 9.7 MB (74.33%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int> freq;
+        for(char c:s){
+            freq[c]++;
+        }
+        for(char c:t){
+            freq[c]--;
+
+         if (freq[c]<0){
+            return false;
+        }
+        }
+    return true;
+    }
+};
