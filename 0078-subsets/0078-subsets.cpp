6904755bcc313c6d052992ce783ceabf629993a8
suchitashukla123 <2k23.cscys2311174@gmail.com>
2025-09-14 23:21:10 +0530

    Time: 3 ms (31.54%), Space: 9.7 MB (94.93%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        backtrack(nums, 0, current, result);
+        return result;
+    }
+
+private:
+    void backtrack(vector<int>& nums, int start, vector<int>& current, vector<vector<int>>& result) {
+        result.push_back(current); // add current subset
+
+        for (int i = start; i < nums.size(); i++) {
+            current.push_back(nums[i]);           // include nums[i]
+            backtrack(nums, i + 1, current, result); // explore further
+            current.pop_back();                   // backtrack
+        }
+    }
+};
