ac951532aceaaf2be16e5f700f6d9f29eed0ee06
suchitashukla123 <2k23.cscys2311174@gmail.com>
2025-10-07 22:08:21 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.73%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x; // 0 or 1 case
+        
+        int left = 1, right = x / 2, ans = 0;
+        
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long sq = mid * mid;
+            
+            if (sq == x) return mid;
+            else if (sq < x) {
+                ans = mid;  // store possible answer
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
