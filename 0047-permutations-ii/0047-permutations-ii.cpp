4a47e7e36d3405068caf96baacac1b111e874aca
suchitashukla123 <2k23.cscys2311174@gmail.com>
2025-11-03 23:12:05 +0530

    Time: 3 ms (65.99%), Space: 15.1 MB (18.87%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        backtrack(nums, 0);
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end()); // sorting helps group duplicates
+        backtrack(nums, 0, res);
         return res;
     }
     
-    void backtrack(vector<int>& nums, int start) {
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& res) {
         if (start == nums.size()) {
             res.push_back(nums);
             return;
         }
         
+        unordered_set<int> used; // to track duplicates at this recursion depth
+        
         for (int i = start; i < nums.size(); ++i) {
-            // skip duplicates by checking sorted order
-            if (i > start && nums[i] == nums[start]) continue;
+            // skip duplicates at the same recursion level
+            if (used.count(nums[i])) continue;
+            used.insert(nums[i]);
             
-            swap(nums[i], nums[start]);
-            backtrack(nums, start + 1);
-            swap(nums[i], nums[start]);
-            
-            // restore sorted order for next iteration
-            sort(nums.begin() + start + 1, nums.end());
+            swap(nums[start], nums[i]);             // fix one element
+            backtrack(nums, start + 1, res);        // recurse on the rest
+            swap(nums[start], nums[i]);             // backtrack
         }
     }
 };
+
